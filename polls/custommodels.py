f514eb06682920ba8ced250b845741e2bd4cd339
Louis NGUYEN <louis@Nguyens-MacBook-Pro.local>
2018-07-27 21:37:30 +0200

    updated custommodels : enable updateQuestion with a list, autogenerate id

--- polls/custommodels.py
+++ polls/custommodels.py
@@ -5,8 +5,10 @@ class Question:
 		self.client = MongoClient('127.0.0.1', 27017)
 		self.data = self.client.db.question
 
-	def createQuestion(self, ide, question_type, question_text, answers, images = [], video = None):
-		"""createQuesiton(id, type, text, answers, images, video)"""
+	def createQuestion(self, question_type, question_text, answers, images = [], video = None):
+		"""createQuesiton(type, text, answers, images, video)"""
+		count = self.data.estimated_document_count() + 1
+		ide = 'q' + str(count)
 		stuff = {
 			'id' : ide,
 			'type' : question_type,
@@ -18,7 +20,7 @@ class Question:
 
 		self.data.insert_one(stuff)
 
-	def updateQuestion(self, ide ,what_to_update, new):
+	def updateQuestion(self, ide , what_to_update):
 		"""updateQuestion(id , what_to_update, new)
 
 		what_to_update -> string.   for example : 'id', 'type', 'text'
@@ -26,7 +28,5 @@ class Question:
 
 		stuff = self.data.find_one({'id' : ide})
 
-		self.data.update_one({'id' : ide}, {'$set' :  {
-				what_to_update : new
-			}
+		self.data.update_one({'id' : ide}, {'$set' : what_to_update
 		})
