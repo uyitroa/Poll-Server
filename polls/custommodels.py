3b4cef5215a6417689536259fc8eb3ec442d30cb
yuitora <30695483+shintaroDesu@users.noreply.github.com>
2018-07-29 00:38:41 +0200

    add create, update, getId of answer

--- polls/custommodels.py
+++ polls/custommodels.py
@@ -6,7 +6,7 @@ class Question:
 		self.data = self.client.db.question
 
 	def createQuestion(self, question_json):
-		"""createQuesiton(type, text, answers, images, video)"""
+		"""createQuestion(type, text, answers, images, video)"""
 		count = self.data.estimated_document_count() + 1
 		ide = 'q' + str(count)
 		self.data.insert_one(question_json)
@@ -19,8 +19,7 @@ class Question:
 
 		stuff = self.data.find_one({'id' : ide})
 
-		self.data.update_one({'id' : ide}, {'$set' : what_to_update
-		})
+		self.data.update_one({'id' : ide}, {'$set' : what_to_update})
 
 	def getQuestionById(self, ide):
 		"""getQuestionById(id)"""
@@ -36,3 +35,31 @@ class Question:
 		Argument example: {'id' : 'q1'} or {'id' : 'q1'}, 'id' : 'q2'}"""
 		for x in args:
 			self.data.remove(x)
+
+class Answer:
+	def __init__(self):
+		self.client = MongoClient('127.0.0.1', 27017)
+		self.data = self.client.db.answer
+		
+	def createAnswer(self, answer_json):
+		"""createAnswer()"""
+		count = self.data.estimated_document_count() + 1
+		ide = "a" + str(count)
+		self.data.insert_one(answer_json)
+		
+	def updateAnswer(self, ide, to_update):
+		"""updateAnswer(id , to_update, new)
+
+		to_update -> string.   for example : 'id', 'type', 'text'
+		"""
+		stuff = self.data.find_one({'id' : ide})
+
+		self.data.update_one({'id' : ide}, {'$set' : to_update})
+	
+	def getAnswerById(self, ide):
+		"""getAnswerById(id)"""
+		a = self.data.find_one({"id" : ide})
+		if a != None:
+			return a["answer"]
+		else:
+			return None
