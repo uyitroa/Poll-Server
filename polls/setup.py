768b722281c54886587cb40bf235c870ebe2e4e4
shintaroDesu <snkraishin87@gmail.com>
2018-07-29 00:16:34 +0000

    update setup.py views.py url

---
+++ polls/setup.py
@@ -0,0 +1,6 @@
+from .custommodels import Question
+global_question_class = Question()
+def setglobal():
+	global global_question_class
+
+setglobal()
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from .custommodels import Question, Answer
+from .setup import *
 
 # Create your views here.
+def error():
+	return HttpResponse("Error")
+
+def getQuestion(request, ide):
+	try:
+		question_json = global_question_class.getQuestionById(ide)
+		return JsonResponse(question_json)
+	except Exception as e:
+		print(e)
+		return error()
+
+def submitAnswer(request):
+	try:
+		pass
+	except Exception as e:
+		print(e)
+		return error()
