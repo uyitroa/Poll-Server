8f2e2f11f0163c87017441ba1f33a967710d532f
yuitora <30695483+shintaroDesu@users.noreply.github.com>
2018-07-31 21:02:41 +0200

    updated config.py

--- PollServer/config.py
+++ PollServer/config.py
@@ -1,13 +1,5 @@
-from pymongo import MongoClient
-
 #CONFIF
 CLIENT = MongoClient('127.0.0.1', 27017)
 PORT_NAME = 8000
 HOST_NAME = '0.0.0.0'
-DATABASE = CLIENT.db
-
-
-#GLOBALIZE
-def set_global():
-	global PORT_NAME, HOST_NAME, DATABASE
-set_global()
+DATABASE = 'db'
